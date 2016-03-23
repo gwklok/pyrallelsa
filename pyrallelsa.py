33185efc05910caf4696c98b8f2ba9bfd712a41f
hfaran <hamzafaran@outlook.com>
2016-03-23 06:06:33 -0700

    Add initial (untested) implementation

---
+++ pyrallelsa.py
@@ -0,0 +1,107 @@
+from __future__ import division
+
+from abc import abstractmethod, abstractproperty
+import random
+import time
+from multiprocessing import cpu_count, Pool
+from collections import namedtuple
+from importlib import import_module
+
+import simanneal
+
+
+class State(object):
+
+    @abstractmethod
+    def copy(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def serialize(self):
+        raise NotImplementedError
+
+    @classmethod
+    def load(cls, s):
+        raise NotImplementedError
+
+
+class Annealer(simanneal.Annealer):
+    pass
+
+
+class ProblemSet(object):
+
+    @abstractmethod
+    def divide(self):
+        raise NotImplementedError
+
+    @abstractproperty
+    def problem_data_str(self):
+        raise NotImplementedError
+
+    @abstractproperty
+    def pcp(self):
+        raise NotImplementedError
+
+    @abstractproperty
+    def psp(self):
+        raise NotImplementedError
+
+
+ProblemClassPath = namedtuple('ProblemClassPath', ['module', 'cls'])
+ProblemStatePath = namedtuple('ProblemStatePath', ['module', 'cls'])
+Solution = namedtuple('Solution', ['state', 'energy'])
+
+def runner((id, pcp, psp, serialized_state, minutes, problem_data)):
+    pscls_module = import_module(psp.module)
+    PSCls = getattr(pscls_module, psp.cls)
+    state = PSCls.load(serialized_state)
+
+    pccls_module = import_module(pcp.module)
+    PCCls = getattr(pccls_module, pcp.cls)
+    annealer = PCCls(state, problem_data)
+    auto_schedule = annealer.auto(minutes=minutes)
+    annealer.set_schedule(auto_schedule)
+    best_state, best_fitness = annealer.anneal()
+
+    return Solution(best_state.serialize(), best_fitness)
+
+
+class ParallelSAManager(object):
+    """ParallelSAManager
+
+    :type problem_set: ProblemSet
+    """
+    def __init__(self, problem_set):
+        self.problem_set = problem_set
+
+    def run(self, minutes):
+        start = time.time()
+        process_pool = Pool()
+
+        subproblems = self.problem_set.divide()
+        cpus = cpu_count()
+        available_cpu_time = minutes*cpus
+        time_per_task = available_cpu_time/len(subproblems)
+
+        pcp = self.problem_set.pcp
+        psp = self.problem_set.psp
+        problem_data = self.problem_set.problem_data_str
+        solutions = process_pool.map(
+            runner,
+            [
+                (i, pcp, psp, state.serialize(), time_per_task,
+                 problem_data) for i, state in
+                enumerate(self.problem_set.divide())
+            ]
+        )
+
+        winner = sorted(solutions, key=lambda s: s.fitness, reverse=True)[0]
+
+        print("With an energy of {}; {} was the best.".format(
+            winner.energy,
+            winner.state
+        ))
+        print("Run took {}s".format(time.time() - start))
+
+        return winner
---
+++ tsp.py
@@ -0,0 +1,118 @@
+import random
+import math
+import json
+
+from pyrallelsa import Annealer
+from pyrallelsa import State
+from pyrallelsa import ProblemSet, ProblemClassPath, ProblemStatePath
+
+
+class TSPState(State):
+
+    def __init__(self, route, locked_range=0):
+        self.route = route
+        self.locked_range = locked_range
+
+    def copy(self):
+        return TSPState(route=self.route, locked_range=self.locked_range)
+
+    def serialize(self):
+        kwargs = {"route": self.route, "locked_range": self.locked_range}
+        return json.dumps(kwargs)
+
+    @classmethod
+    def load(cls, s):
+        return cls(**json.loads(s))
+
+
+def distance(a, b):
+    """Calculates distance between two latitude-longitude coordinates."""
+    R = 3963  # radius of Earth (miles)
+    # print("a: {} b: {}".format(a,b))
+    try:
+        lat1, lon1 = math.radians(a[0]), math.radians(a[1])
+        lat2, lon2 = math.radians(b[0]), math.radians(b[1])
+    except:
+        raise
+    return math.acos(math.sin(lat1) * math.sin(lat2) +
+                     math.cos(lat1) * math.cos(lat2) * math.cos(lon1 - lon2)) * R
+
+
+def get_distance_matrix(cities):
+    # create a distance matrix
+    distance_matrix = {}
+    for ka, va in cities.items():
+        distance_matrix[ka] = {}
+        for kb, vb in cities.items():
+            if kb == ka:
+                distance_matrix[ka][kb] = 0.0
+            else:
+                distance_matrix[ka][kb] = distance(va, vb)
+    return distance_matrix
+
+
+class TSPProblem(Annealer):
+    """Traveling Salesman Problem Annealer
+    :param dict job_data: Unused currently
+    :param State state: state of the current annealer process
+    """
+    def __init__(self, state, problem_data):
+        self.cities = problem_data["cities"]
+        self.distance_matrix = problem_data["distance_matrix"]
+        super(TSPProblem, self).__init__(state)  # important!
+
+    def move(self, state=None):
+        """Swaps two cities in the route.
+
+        :type state: TSPState
+        """
+        state = self.state if state is None else state
+        a = random.randint(state.locked_range, len(state) - 1)
+        b = random.randint(state.locked_range, len(state) - 1)
+        state[a], state[b] = state[b], state[a]
+
+    def energy(self, state=None):
+        """Calculates the length of the route."""
+        state = self.state if state is None else state
+        e = 0
+        for i in range(len(state)):
+            e += self.distance_matrix[state[i-1]][state[i]]
+        return e
+
+    def copy_state(self, state):
+        """Return copy of state
+
+        :type state: TSPState
+        :rtype: State
+        """
+        return state.copy()
+
+
+class TSPProblemSet(ProblemSet):
+    def __init__(self, cities, start_city=None):
+        self.cities = cities
+        self.start_city = self.cities[0] if start_city is None else start_city
+        self.distance_matrix = get_distance_matrix(cities)
+        self._problem_data = {"cities": self.cities,
+                              "distance_matrix": self.distance_matrix}
+        self._problem_data_str = json.dumps(self._problem_data)
+
+    def problem_data_str(self):
+        return self._problem_data_str
+
+    def pcp(self):
+        return ProblemClassPath("tsp", "TSPProblem")
+
+    def psp(self):
+        return ProblemStatePath("tsp", "TSPState")
+
+    def divide(self):
+        for city in self.cities:
+            if city == self.start_city:
+                continue
+            cities = self.cities[:]
+            cities.remove(self.start_city)
+            cities.remove(city)
+            random.shuffle(cities)
+            route = [self.start_city, city] + cities
+            yield TSPState(route=route, locked_range=2)
