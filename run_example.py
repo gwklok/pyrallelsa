5c36a43f65b32c87fd2af49ffff8e50c72cfccd5
hfaran <hamzafaran@outlook.com>
2016-03-26 20:55:07 -0700

    add test for seq

--- run_example.py
+++ run_example.py
@@ -1,16 +1,39 @@
-from pyrallelsa import ParallelSAManager, ProblemClassPath
-from pyrallelsa.examples.tsp.cities import cities_120
+#!/usr/bin/env python
+
+import json
+
+import click
+import time
+
+from pyrallelsa import ParallelSAManager, ProblemClassPath, runner
+from pyrallelsa.examples.tsp.cities import cities_120, cities_20
 
 
-def main():
+@click.command()
+@click.option('-t', '--type', type=click.Choice(['seq', 'mc']), default='seq')
+def main(type):
+    cities = cities_20
+
     problem_data = dict(
-        cities=cities_120,
+        cities=cities,
         start_city="Houston",
-        updates_enabled=False
+        updates_enabled=True
     )
     pcp = ProblemClassPath("pyrallelsa.examples.tsp", "TSPProblem")
-    psam = ParallelSAManager(pcp, problem_data)
-    psam.run(minutes=2.0, cpus=3)
+
+    if type == 'mc':
+        psam = ParallelSAManager(pcp, problem_data)
+        psam.run(minutes=2.0, cpus=3)
+    elif type == 'seq':
+        start = time.time()
+        args = (0, pcp, json.dumps(cities.keys()), 8.0,
+                json.dumps(problem_data), None)
+        winner = runner(args)
+        print("With an energy of {}; {} was the best.".format(
+            winner.energy,
+            winner.state
+        ))
+        print("Run took {}s".format(time.time() - start))
 
 
 if __name__ == '__main__':
